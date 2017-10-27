85784212d25ecccade696a25107cada52fbcb53f
snam4 <snam4@saic.edu>
2017-10-27 12:08:45 -0400

    W9-IsTriggerDemo
    Intro to scripting in Unity

---
+++ w9/IsTriggerDemo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unityAnimation : MonoBehaviour {
+
+	//OnTriggerEnter  : OnTriggerStay : OnTriggerExit
+	void OnTriggerEnter(Collider other){
+		GetComponent<Renderer> ().material.color = Color.cyan;
+	}
+	void OnTriggerStay(){
+		transform.Rotate(Vector3.up, Time.deltaTime *30, Space.World);
+	}
+	void OnTriggerExit(Collider other){
+		GetComponent<Renderer> ().material.color = Color.white;
+	}
+}
