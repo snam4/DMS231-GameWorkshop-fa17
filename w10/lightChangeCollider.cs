14431a29bc0a7806a612a4c74996ed9d6c860127
snam4 <snam4@saic.edu>
2017-10-31 22:24:39 -0400

    w10-sunlight-trigger

---
+++ w10/lightChangeCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightChangeCollider : MonoBehaviour {                   
+	public GameObject sun;
+	private Color c = new Color(.5f, .0f, .0f);
+
+	void OnTriggerEnter(){
+		sun.GetComponent<Light>().intensity = 0.7f;
+		sun.GetComponent<Light> ().color = c;
+	}
+
+	void OnTriggerExit(){
+		sun.GetComponent<Light>().intensity = 1.1f;
+		sun.GetComponent<Light> ().color = new Color(1,1, 1);
+	}
+}
