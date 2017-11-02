873b9bb0186d147e22b916eb81762e23751a23a8
snam4 <snam4@saic.edu>
2017-11-02 11:49:05 -0400

    w10-CoinPickUp-Demo

---
+++ w10/extra-demo/CoinPickUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickUp : MonoBehaviour {
+	private GameObject player;
+	public playerScore pScore;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		pScore = player.GetComponent<playerScore> ();
+	}
+	void Update(){
+		transform.Rotate (Vector3.up, Time.deltaTime * 20, Space.World);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Player") {
+			pScore.score++;
+			Destroy (gameObject);	
+		}
+	}
+}
