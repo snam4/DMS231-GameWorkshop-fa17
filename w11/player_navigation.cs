b90e46b0f78bb063b6aeb1f16fde093a3968c9a5
snam4 <snam4@saic.edu>
2017-11-07 12:53:05 -0500

    w11-player_navigation

---
+++ w11/player_navigation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_navigation : MonoBehaviour {
+	private float speed = 7.0f;
+
+	void Update () {
+		float r = Input.GetAxis ("Horizontal") *30* Time.deltaTime;
+		transform.Rotate (Vector3.up, r , Space.Self);
+		float z = Input.GetAxis ("Vertical") *Time.deltaTime*speed;
+		transform.Translate (0, 0, z);
+	}
+}
